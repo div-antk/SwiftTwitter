027b06de3fe3ff9477e92e562e3b9dd64db51570
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-21 20:58:46 +0900

    LoginControllerの編集

--- SwiftTwitter/Controller/LoginViewController.swift
+++ SwiftTwitter/Controller/LoginViewController.swift
@@ -6,16 +6,42 @@
 //
 
 import UIKit
+import FirebaseAuth
+import NVActivityIndicatorView
 
 class LoginViewController: UIViewController {
 
+    var provider:OAuthProvider
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.provider = OAuthProvider(providerID: TwitterAuthProviderID)
+        provider.customParameters = ["lang":"ja"]
+        
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // ログインしたあとに戻れるように消す
+        self.navigationController?.isNavigationBarHidden = true
+    }
 
+    @IBAction func twitterLogin(_ sender: Any) {
+        self.provider = OAuthProvider(providerID: TwitterAuthProviderID)
+        
+        // 強制ログイン
+        provider.customParameters = ["force_login":"true"]
+        provider.getCredentialWith(nil) { (credential, error) in
+            
+            // ActivityIndicatorViewを回す
+            
+            // ログインの処理
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
