c6c0f982f2db070d311fb866f10b5bee965d935f
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-21 21:35:31 +0900

    ライブラリの初期化など

--- SwiftTwitter/Controller/ViewController.swift
+++ SwiftTwitter/Controller/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import BubbleTransition
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, XMLParserDelegate, UIViewControllerTransitioningDelegate {
     
     var userName = String()
     
     @IBOutlet weak var meigenLabel: UILabel!
     
+    // プラスボタン
     @IBOutlet weak var toFeedButton: UIButton!
+    
+    let db = Firestore.firestore()
+    let transition = BubbleTransition()
+    let interactiveTransition = BubbleInteractiveTransition()
+    var parser = XMLParser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
