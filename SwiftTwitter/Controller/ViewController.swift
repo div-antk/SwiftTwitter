8cdab2f21d8cc2f91fb97ed37a140f71759a55b6
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-22 18:58:36 +0900

    XML解析を行う

--- SwiftTwitter/Controller/ViewController.swift
+++ SwiftTwitter/Controller/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 import BubbleTransition
 import Firebase
 
+class FeedItem {
+    var meigen:String!
+    var auther:String!
+}
+
 class ViewController: UIViewController, XMLParserDelegate, UIViewControllerTransitioningDelegate {
     
     var userName = String()
@@ -28,6 +33,32 @@ class ViewController: UIViewController, XMLParserDelegate, UIViewControllerTrans
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // ボタンを丸くする
+        toFeedButton.layer.cornerRadius = toFeedButton.frame.width/2
+        
+        // ナビゲーションバーを消す
+        self.navigationController?.isNavigationBarHidden = true
+        
+        // XML解析を行う
+        let url = "http://meigen.doodlenote.net/api?c=1"
+        let urlToSend = URL(string: url)
+        parser = XMLParser(contentsOf: urlToSend!)!
+        parser.delegate = self
+        parser.parse()
+    }
+    
+    @IBAction func share(_ sender: Any) {
+        
+    }
+    
+    
+    @IBAction func sendData(_ sender: Any) {
+        // Firestoreで値を保存する
+        if let quote = meigenLabel.text, let userName = Auth.auth()
+    }
     
 }
 
