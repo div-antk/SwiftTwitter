d0200ab9402972c31188234c61a21678a243ba59
div-antk <51283468+owaii@users.noreply.github.com>
2021-01-23 18:35:21 +0900

    プロトコルの準備

--- SwiftTwitter/Controller/FeedViewController.swift
+++ SwiftTwitter/Controller/FeedViewController.swift
@@ -12,23 +12,44 @@ import SDWebImage
 import ViewAnimator
 import FirebaseFirestore
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var interactiveTransition:BubbleInteractiveTransition
+    
+    @IBOutlet weak var tableView: UITableView!
+    let db = Firestore.firestore()
+    
+    var feeds:[Feeds] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // カスタムセル
+        
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            <#code#>
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            <#code#>
+        }
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
---
+++ SwiftTwitter/Model/Feeds.swift
@@ -0,0 +1,14 @@
+//
+//  Feeds.swift
+//  SwiftTwitter
+//
+//  Created by Takuya Ando on 2021/01/23.
+//
+
+import Foundation
+
+struct Feeds {
+    let userName:String
+    let quate:String
+    let profileURL:String
+}
